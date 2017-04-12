77760ec617f75f6eb609053315ef719adc717dcb
Jannik Kolodziej <github@jkolodziej.de>
2017-04-12 18:51:58 +0200

    initial commit from Spring Initializr

---
+++ src/main/java/eu/kandru/luna/Ts3lunaApplication.java
@@ -0,0 +1,12 @@
+package eu.kandru.luna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ts3lunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ts3lunaApplication.class, args);
+    }
+}
---
+++ src/test/java/eu/kandru/luna/Ts3lunaApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.kandru.luna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ts3lunaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
