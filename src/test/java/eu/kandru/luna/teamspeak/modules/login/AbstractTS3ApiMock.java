f516690be4ff977ea753845bec2a0f05691745ef
Jonas Vothknecht <jonsen@kandru.de>
2017-05-23 00:32:58 +0200

    Enhanced login module tests, better mocking for the ts3api - Added abstract class which takes care of mocking the ts3api for test classes - Changed the login module tests so that they use the new ts3api mocking class

---
+++ src/test/java/eu/kandru/luna/teamspeak/modules/login/AbstractTS3ApiMock.java
@@ -0,0 +1,141 @@
+package eu.kandru.luna.teamspeak.modules.login;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import com.github.theholywaffle.teamspeak3.TS3ApiAsync;
+import com.github.theholywaffle.teamspeak3.api.CommandFuture;
+import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+import com.github.theholywaffle.teamspeak3.api.wrapper.ClientInfo;
+import com.github.theholywaffle.teamspeak3.api.wrapper.DatabaseClient;
+import com.github.theholywaffle.teamspeak3.api.wrapper.DatabaseClientInfo;
+
+import eu.kandru.luna.teamspeak.TS3Manager;
+import eu.kandru.luna.teamspeak.TS3Properties;
+
+public abstract class AbstractTS3ApiMock {
+
+    @Mock protected TS3Manager manager;
+    @Mock protected TS3Properties properties;
+    @Mock protected TS3ApiAsync ts3api;
+	
+    @Spy protected CommandFuture<List<Client>> clientList;
+    protected List<CommandFuture<ClientInfo>> clientInfoFutures;
+    protected List<ClientInfo> mockedClientInfos;
+    protected List<Client> mockedClients;
+    
+    @Spy protected CommandFuture<List<DatabaseClient>> dbClientList;
+    protected List<CommandFuture<DatabaseClientInfo>> dbClientInfoFutures;
+    protected List<DatabaseClientInfo> mockedDbClientInfos;
+    protected List<DatabaseClient> mockedDbClients;    
+    
+    private static final int DEFAULT_REQUEST_TIMEOUT = 500;
+    private static final int DEFAULT_SERVER_PORT = 4242;
+    private static final String DEFAULT_TS3_IP = "192.168.42.42";
+    
+    
+    protected void setup() throws InterruptedException, TimeoutException{
+    	MockitoAnnotations.initMocks(this);
+    	mockedClients  = new ArrayList<>();
+    	mockedClientInfos  = new ArrayList<>();
+    	mockedDbClients = new ArrayList<>();
+    	mockedDbClientInfos = new ArrayList<>();
+    	clientInfoFutures = new ArrayList<>();
+    	dbClientInfoFutures = new ArrayList<>();
+    	
+        when(manager.getTs3api()).thenReturn(ts3api);
+        when(ts3api.getClients()).thenReturn(clientList);
+        when(ts3api.getDatabaseClients()).thenReturn(dbClientList);
+        
+        //Mock spies
+        doReturn(mockedClients).when(clientList).get();
+        doReturn(mockedClients).when(clientList).get(anyLong(),any());
+        doReturn(mockedDbClients).when(dbClientList).get();
+        doReturn(mockedDbClients).when(dbClientList).get(anyLong(),any());
+        
+        when(properties.getRequestTimeout()).thenReturn(DEFAULT_REQUEST_TIMEOUT);
+        when(properties.getIp()).thenReturn(DEFAULT_TS3_IP);
+        when(properties.getPort()).thenReturn(DEFAULT_SERVER_PORT);
+    }
+    
+    
+    //Clients will be named Client0, Client1 ...
+    //Clients ips will be 192.168.0.i with i as the client nr 0,1,...
+    //Clients id will be 0,1,...
+    //Clients uids are UniqueIdentifier0, UniqueIdentifier1, ...
+    //This will also call add db clients
+    protected void addClients(int count) throws InterruptedException, TimeoutException{
+    	for (int i = 0; i < count; i++){    		
+    		ClientInfo mockClient = mock(ClientInfo.class);
+    		mockedClients.add(mockClient);
+    		mockedClientInfos.add(mockClient);
+    		
+    		when(mockClient.getId()).thenReturn(i);
+    		when(mockClient.getIp()).thenReturn("192.168.0."+i);
+    		when(mockClient.getNickname()).thenReturn("Client"+i);
+    		when(mockClient.getUniqueIdentifier()).thenReturn("UniqueIdentifier"+i);
+    		
+			integrateClient(mockClient);
+    	}
+    	addDatabaseClients(count);
+    }
+    
+  //Clients db id is 0,1,...
+  //Clients last ips are 192.168.0.i with i as the client nr 0,1,...
+  //Clients names are Client0, Client1 ...
+  //Clients uids are UniqueIdentifier0, UniqueIdentifier1, ... 
+    protected void addDatabaseClients(int count) throws InterruptedException, TimeoutException{
+    	for (int i = 0; i < count; i++){
+    		DatabaseClientInfo mockClient = mock(DatabaseClientInfo.class);
+    		mockedDbClients.add(mockClient);
+    		mockedDbClientInfos.add(mockClient);
+    		
+    		when(mockClient.getDatabaseId()).thenReturn(i);
+    		when(mockClient.getLastIp()).thenReturn("192.168.0."+i);
+    		when(mockClient.getNickname()).thenReturn("Client"+i);
+    		when(mockClient.getUniqueIdentifier()).thenReturn("UniqueIdentifier"+i);
+    		
+    		integrateDatabaseClient(mockClient);
+    		
+    		}
+    }
+    
+
+
+	protected void integrateClient(ClientInfo mockedClient) throws InterruptedException, TimeoutException {
+		@SuppressWarnings("unchecked")
+		CommandFuture<ClientInfo> spyCommandFuture = spy(CommandFuture.class);
+		//you can use those to get the client info and trigger the success method with set
+		doReturn(mockedClient).when(spyCommandFuture).get();
+		doReturn(mockedClient).when(spyCommandFuture).get(anyLong(),any());
+		clientInfoFutures.add(spyCommandFuture);
+		
+		when(ts3api.getClientInfo(mockedClient.getId())).thenReturn(spyCommandFuture);
+		when(ts3api.getClientByUId(mockedClient.getUniqueIdentifier())).thenReturn(spyCommandFuture);
+	}
+    
+    protected void integrateDatabaseClient(DatabaseClientInfo mockedClient) throws InterruptedException, TimeoutException{
+    	@SuppressWarnings("unchecked")
+		CommandFuture<DatabaseClientInfo> spyCommandFuture = spy(CommandFuture.class);
+		//you can use those to get the client info and trigger the success method with set
+		doReturn(mockedClient).when(spyCommandFuture).get();
+		doReturn(mockedClient).when(spyCommandFuture).get(anyLong(),any());
+		dbClientInfoFutures.add(spyCommandFuture);
+		
+		when(ts3api.getDatabaseClientInfo(mockedClient.getDatabaseId())).thenReturn(spyCommandFuture);
+		when(ts3api.getDatabaseClientByUId(mockedClient.getUniqueIdentifier())).thenReturn(spyCommandFuture);
+	
+    }
+}
--- src/test/java/eu/kandru/luna/teamspeak/modules/login/TS3LoginModuleTest.java
+++ src/test/java/eu/kandru/luna/teamspeak/modules/login/TS3LoginModuleTest.java
@@ -1,19 +1,12 @@
 package eu.kandru.luna.teamspeak.modules.login;
 
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is; // only is(Class) is deprecated
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
@@ -23,99 +16,59 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.github.theholywaffle.teamspeak3.TS3ApiAsync;
 import com.github.theholywaffle.teamspeak3.api.CommandFuture;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
-import com.github.theholywaffle.teamspeak3.api.wrapper.ClientInfo;
-import com.github.theholywaffle.teamspeak3.api.wrapper.DatabaseClientInfo;
-
-import eu.kandru.luna.teamspeak.TS3Manager;
-import eu.kandru.luna.teamspeak.TS3Properties;
 
 @RunWith(SpringRunner.class)
-public class TS3LoginModuleTest {
+public class TS3LoginModuleTest extends AbstractTS3ApiMock{
 
-    @Mock TS3Manager manager;
-    @Mock TS3Properties properties;
-    @Mock TS3ApiAsync ts3api;
-    @Mock CommandFuture<List<Client>> dbClients;
-    @Mock Client matchingClient;
-    @Mock Client missmatchingClient;
-    @Mock DatabaseClientInfo dbClientInfo;
-    @Mock ClientInfo clientInfo;
-    
-    @Captor ArgumentCaptor<String> messageCaptor;
-    
-    //test get by ip
-    String ipToCheck = "127.0.0.1";
-    List<Client> clients;
-    
-    //test send password
-    CommandFuture<DatabaseClientInfo> dbClientInfoFuture;
-    CommandFuture<ClientInfo> clientInfoFuture;
-    CommandFuture<Boolean> privateMessageFuture;
-    String clientUid = "some_uid";
-    int clientDbId = 1337;
-    int clientId = 42;
-    String clientNickname = "Clientnick";
-    String password = "123456";
-    
+	@Captor protected ArgumentCaptor<String> messageCaptor;
+	
     @InjectMocks
-    TS3LoginModule systemUnderTest;
+    private TS3LoginModule systemUnderTest;
+
+	private CommandFuture<Boolean> privateMessageFuture;
+	
+	private static final int CLIENT_COUNT_TO_TEST = 10;
+	private static final String PASSWORD = "some_password";
+	private static final int DEFAULT_CLIENT_TO_TEST = 3; //0 > DEFAULT_..._TEST < CLIENT_COUNT_TO_TEST
     
     @Before
     public void setUp() throws InterruptedException, TimeoutException{
-        MockitoAnnotations.initMocks(this);
-        
-        clients = new ArrayList<>();
-        clients.add(matchingClient);
-        clients.add(missmatchingClient);
-        
-        dbClientInfoFuture = new CommandFuture<>();
-        clientInfoFuture = new CommandFuture<>();
+    	super.setup();
+    	MockitoAnnotations.initMocks(this);
+
         privateMessageFuture = new CommandFuture<>();
         
-        when(matchingClient.getIp()).thenReturn(ipToCheck);
-        when(missmatchingClient.getIp()).thenReturn("wrong ip");
-        when(manager.getTs3api()).thenReturn(ts3api);
-        when(ts3api.getClients()).thenReturn(dbClients);
-        when(dbClients.get(anyLong(),any())).thenReturn(clients);
-        when(properties.getRequestTimeout()).thenReturn(500);
-        
-        when(ts3api.getDatabaseClientInfo(eq(clientDbId))).thenReturn(dbClientInfoFuture);
-        when(ts3api.getClientByUId(eq(clientUid))).thenReturn(clientInfoFuture);
-        when(dbClientInfo.getUniqueIdentifier()).thenReturn(clientUid);
-        when(clientInfo.getId()).thenReturn(clientId);
-        when(clientInfo.getUniqueIdentifier()).thenReturn(clientUid);
-        when(clientInfo.getNickname()).thenReturn(clientNickname);
         when(ts3api.sendPrivateMessage(anyInt(), any())).thenReturn(privateMessageFuture);
     }
     
     @Test
-    public void testGetClientsByIp() throws TimeoutException{
-        List<Client> foundClients = systemUnderTest.getClientsByIp(ipToCheck);
-        assertThat(foundClients, is(not(empty())));
-        assertThat(foundClients.size(), is(equalTo(1)));
-        assertThat(foundClients.get(0), is(equalTo(matchingClient)));
+    public void testGetClientsByIp() throws TimeoutException, InterruptedException{
+    	addClients(CLIENT_COUNT_TO_TEST);
+        List<Client> foundClients = systemUnderTest.getClientsByIp(mockedClients.get(DEFAULT_CLIENT_TO_TEST).getIp());
+        assertThat(foundClients).isNotEmpty();
+        assertThat(foundClients.size()).isEqualTo(1);
+        assertThat(foundClients.get(0)).isEqualTo(mockedClients.get(DEFAULT_CLIENT_TO_TEST));
     }
     
     @Test
-    public void testSendPasswordToUser() {
-        systemUnderTest.sendPasswordToUser(clientDbId, password);
+    public void testSendPasswordToUser() throws InterruptedException, TimeoutException {
+    	addClients(CLIENT_COUNT_TO_TEST);
+    	systemUnderTest.sendPasswordToUser(mockedDbClients.get(DEFAULT_CLIENT_TO_TEST).getDatabaseId(), PASSWORD);
         
-        dbClientInfoFuture.set(dbClientInfo);
-        clientInfoFuture.set(clientInfo);
-        privateMessageFuture.set(null);
+    	verify(ts3api).getDatabaseClientInfo(mockedDbClients.get(DEFAULT_CLIENT_TO_TEST).getDatabaseId());
+    	
+        dbClientInfoFutures.get(DEFAULT_CLIENT_TO_TEST).set(mockedDbClientInfos.get(DEFAULT_CLIENT_TO_TEST));
+        clientInfoFutures.get(DEFAULT_CLIENT_TO_TEST).set(mockedClientInfos.get(DEFAULT_CLIENT_TO_TEST));
+        privateMessageFuture.set(true);
         
-        verify(ts3api).sendPrivateMessage(eq(42), messageCaptor.capture());
+        verify(ts3api).sendPrivateMessage(eq(mockedClients.get(DEFAULT_CLIENT_TO_TEST).getId()), messageCaptor.capture());
         
-        if(!messageCaptor.getValue().contains(password)){
-            fail("Password message did not contain password. Expected password \""+password+"\", received message \""+messageCaptor.getValue()+"\".");
-        }
+        assertThat(messageCaptor.getValue()).contains(PASSWORD);
     }
     
 }
