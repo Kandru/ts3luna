dab6b1e2d2ca2146dff1e992867bb1f8d8fe7411
Jannik Kolodziej <github@jkolodziej.de>
2017-10-08 23:27:39 +0200

    commit to test jenkins auto-build

--- src/main/java/eu/kandru/luna/i18n/MessageBuilder.java
+++ src/main/java/eu/kandru/luna/i18n/MessageBuilder.java
@@ -5,7 +5,7 @@ import java.text.MessageFormat;
 public class MessageBuilder {
 
     private static MessageFormat passwordMessage = new MessageFormat("\nHi {0},\n" +
-            "someone, hopefully you, tries to use your identity to log into my webinterface. \n" +
+                                                                             "someone, hopefully you, tries to use your identity to log into my web interface. \n" +
             "If it''s actually you, use[b]{1}[/b] as password. " +
             "If not, just ignore this message.");
 
