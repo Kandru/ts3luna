5e84a030c7ff8e7be7312caae65e5a8dd46ae96f
Jannik Kolodziej <github@jkolodziej.de>
2017-10-13 01:22:55 +0200

    fix missing space in password message

--- src/main/java/eu/kandru/luna/i18n/MessageBuilder.java
+++ src/main/java/eu/kandru/luna/i18n/MessageBuilder.java
@@ -4,10 +4,11 @@ import java.text.MessageFormat;
 
 public class MessageBuilder {
 
-    private static MessageFormat passwordMessage = new MessageFormat("\nHi {0},\n" +
-                                                                             "someone, hopefully you, tries to use your identity to log into my web interface. \n" +
-                                                                             "If it''s actually you, use[b]{1}[/b] as password. " +
-                                                                             "If not, just ignore this message.");
+    private static MessageFormat passwordMessage = new MessageFormat(
+            "\nHi {0},\n" +
+                    "someone, hopefully you, tries to use your identity to log into my web interface. \n" +
+                    "If it''s actually you, use [b]{1}[/b] as password. " +
+                    "If not, just ignore this message.");
 
 	public static String generatePasswordMessage(String password, String username){
         return passwordMessage.format(new Object[]{username, password});
