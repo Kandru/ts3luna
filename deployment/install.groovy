ebd37e575bf5b7bc5a959e7810ae589d10096b5b
Jannik Kolodziej <github@jkolodziej.de>
2017-10-12 20:23:53 +0200

    these blacklists where getting out of hand.

--- deployment/install.groovy
+++ deployment/install.groovy
@@ -44,8 +44,7 @@ void addToZip(String prefix, Path path, ZipOutputStream out) {
 
 void installApplication() {
     lunaDir.eachFileRecurse(FileType.FILES) {
-        if( it.fileName ==~ '.*\\.yml' || it.fileName ==~ '.*start\\.sh' || it.fileName ==~ '.*stop\\.sh' || it.fileName ==~ '.*\\.xml' ) { return }
-        Files.delete it
+        if( it.fileName ==~ '.*\\.jar' ) Files.delete it
     }
 
     def delete = []
